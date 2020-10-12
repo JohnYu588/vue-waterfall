cfbcd3db528107bd7da986bdc3e7296701950483
JohnYu <49671649+JohnYu588@users.noreply.github.com>
2020-10-12 16:18:35 +0800

    Update index.vue

--- index.vue
+++ index.vue
@@ -44,10 +44,10 @@
           >
             <img
               class="data-cover"
-              :src="item.cover"
+              :src="$getImagePath(item.thumb)"
               :style="{ width: '100%', height: item.imgHeight + 'px' }"
             />
-            <div class="data-name">{{ item.sign }}</div>
+            <div class="data-name">{{ item.name }}</div>
           </div>
         </div>
       </van-list>
@@ -56,9 +56,16 @@
 </template>
 
 <script>
+import { getActivityList } from '@/global/api/activity/index';
+
 export default {
   data() {
     return {
+      list: [], // 数据列表
+      page: 1, // 页码
+      isRefresh: false, // 下拉刷新
+      limit: 10, // 每页加载条数
+      finishedText: '',
       typeList: [
         // 分类列表
 
@@ -74,8 +81,6 @@ export default {
       typeIndex: 0, // 分类索引
       dataList: [], // 列表数据
       haveData: 0, // 是否有数据，1=无，2=有，0=页面还未初始化
-      pageIndex: 1, // 页码
-      pageSize: 8, // 每页加载数据数量
       isLoading: false, // 下拉刷新进行中，请求开始true, 请求完成false，用于下拉刷新组件van-pull-refresh
       loading: false, // 上拉加载更多中，上拉触底时自动变成true, 请求完成设置为false, 用于列表组件van-list
       finished: false, // 上拉加载是否加载完最后一页数，用于组件van-list
@@ -107,152 +112,56 @@ export default {
       this.typeIndex = index;
       this.onRefresh();
     },
+    initial() {
+      this.page = 1;
+      this.finished = false;
+      this.loading = true;
+      this.dataList = [];
+      this.getList();
+    },
     onRefresh() {
-      // 下拉刷新
-      // if (this.isLoading) return; //还在请求中，返回
-      this.pageIndex = 1; // 重置第一页
-      this.isLoading = true; // 开始加载
-      this.finished = false; // 上拉加载"所有数据已经完成"标识 重置为false
-      // 接口请求
-      this.getDataList();
+      this.initial();
     },
     onBottomLoad() {
       // 上拉加载更多
-      if (this.finished) return; // 说明所有数据已经加载完毕，返回
-      this.getDataList(); // 下一页数据请求中
+      if (this.finished) {
+        return;
+      }
+
+      // 说明所有数据已经加载完毕，返回
+      this.getList(); // 下一页数据请求中
     },
     // 数据请求
-    getDataList() {
+    // 获取活动列表
+    async getList({ page = this.page } = {}) {
+      // 避免下拉刷新时重复加载第一页数据
+      if (!this.loading) {
+        return;
+      }
       try {
-        const list = [
-          {
-            id: 1,
-            cover:
-              'https://pic3.zhimg.com/v2-89735fee10045d51693f1f74369aaa46_r.jpg',
-            sign: '男儿何不带吴钩，收取关山五十州',
-          },
-          {
-            id: 2,
-            cover:
-              'https://pic1.zhimg.com/v2-ca51a8ce18f507b2502c4d495a217fa0_r.jpg',
-            sign: '秦时明月汉时关，万里长征人未还',
-          },
-          {
-            id: 3,
-            cover:
-              'https://pic1.zhimg.com/v2-c90799771ed8469608f326698113e34c_r.jpg',
-            sign:
-              '这俩人是现今世上，手段最高明的摸金校尉，都有万夫不挡之勇，神鬼莫测之机，兼有云长之忠，翼德之猛，子龙之勇，孔明之智',
-          },
-          {
-            id: 4,
-            cover:
-              'https://pic1.zhimg.com/v2-8d3dd83f3a419964687a028de653f8d8_r.jpg',
-            sign: '但使龙城飞将在，不教胡马度阴山',
-          },
-          {
-            id: 5,
-            cover:
-              'https://imgm.gmw.cn/attachement/jpg/site215/20201007/5917432693463665344.jpg',
-            sign: '胡司令,看在党国的份上,拉兄弟一把~ --- 《鬼吹灯》',
-          },
-          {
-            id: 6,
-            cover:
-              'https://imgm.gmw.cn/attachement/jpg/site215/20201007/5917432693463665344.jpg',
-            sign:
-              '看来我要去见马克思了，对不住了战友们，我先走一步，给你们到那边占座位去了，你们有没有什么话要对革命导师说的，我一定替你们转达 --- 《鬼吹灯》',
-          },
-          {
-            id: 7,
-            cover:
-              'https://imgm.gmw.cn/attachement/jpg/site215/20201007/5917432693463665344.jpg',
-            sign:
-              '你看我和老胡，谁能和你把纯洁的革命友谊进一步升华升华？ --- 《鬼吹灯》',
-          },
-          {
-            id: 8,
-            cover:
-              'https://imgm.gmw.cn/attachement/jpg/site215/20201007/7334281492777553585.jpg',
-            sign: '千磨万击还坚劲，任尔东南西北风',
-          },
-          {
-            id: 9,
-            cover:
-              'https://imgm.gmw.cn/attachement/jpg/site215/20201007/7334281492777553585.jpg',
-            sign:
-              '看来我要去见马克思了，对不住了战友们，我先走一步，给你们到那边占座位去了，你们有没有什么话要对革命导师说的，我一定替你们转达 --- 《鬼吹灯》',
-          },
-          {
-            id: 10,
-            cover:
-              'https://imgm.gmw.cn/attachement/jpg/site215/20201007/7334281492777553585.jpg',
-            sign:
-              '老胡同志，你放心去吧，革命事业有你不多，没你不少，你到了老马那边好好学习革命理论啊，听说他们总吃土豆炖牛肉，你吃得习惯吗？ --- 《鬼吹灯》',
-          },
-          {
-            id: 11,
-            cover:
-              'https://imgm.gmw.cn/attachement/jpg/site215/20201007/7334281492777553585.jpg',
-            sign:
-              '老胡同志，你放心去吧，革命事业有你不多，没你不少，你到了老马那边好好学习革命理论啊，听说他们总吃土豆炖牛肉，你吃得习惯吗？ --- 《鬼吹灯》',
-          },
-          {
-            id: 12,
-            cover:
-              'https://imgm.gmw.cn/attachement/jpg/site215/20201007/7334281492777553585.jpg',
-            sign:
-              '老胡同志，你放心去吧，革命事业有你不多，没你不少，你到了老马那边好好学习革命理论啊，听说他们总吃土豆炖牛肉，你吃得习惯吗？ --- 《鬼吹灯》',
-          },
-          {
-            id: 13,
-            cover:
-              'https://imgm.gmw.cn/attachement/jpg/site215/20201007/7334281492777553585.jpg',
-            sign:
-              '老胡同志，你放心去吧，革命事业有你不多，没你不少，你到了老马那边好好学习革命理论啊，听说他们总吃土豆炖牛肉，你吃得习惯吗？ --- 《鬼吹灯》',
-          },
-          {
-            id: 14,
-            cover:
-              'https://imgm.gmw.cn/attachement/jpg/site215/20201007/7334281492777553585.jpg',
-            sign:
-              '老胡同志，你放心去吧，革命事业有你不多，没你不少，你到了老马那边好好学习革命理论啊，听说他们总吃土豆炖牛肉，你吃得习惯吗？ --- 《鬼吹灯》',
-          },
-          {
-            id: 15,
-            cover:
-              'https://imgm.gmw.cn/attachement/jpg/site215/20201007/7334281492777553585.jpg',
-            sign:
-              '老胡同志，你放心去吧，革命事业有你不多，没你不少，你到了老马那边好好学习革命理论啊，听说他们总吃土豆炖牛肉，你吃得习惯吗？ --- 《鬼吹灯》',
-          },
-        ];
-        if (list.length > 0) {
-          // 从list中取pageSize条数据出来
-          let tempList = [];
-          // for (let i = 0; i < this.pageSize; i++) {
-          //   if (list.length > 0) {
-          //     const tempIndex = parseInt(Math.random() * 1000) % list.length;
-          //     tempList.push(list[tempIndex]);
-          //     list.splice(tempIndex, 1);
-          //   }
-          // }
-          tempList = list;
-          this.loadImagesHeight(tempList); // 模拟预加载图片，获取图片高度
+        const { code, data } = await getActivityList({
+          page,
+          pageSize: this.limit,
+        });
+        if (code === 200) {
+          // this.loadImagesHeight(data,data.list); // 模拟预加载图片，获取图片高度
+          this.resolveDataList(data, data.list);
         } else {
-          this.loadImagesHeight(list); // 处理数据
+          // this.loadImagesHeight(data,data.list); // 处理数据
+          this.resolveDataList(data, data.list);
         }
-      } catch (e) {
+      } catch (error) {
         this.$toast.clear();
         this.isLoading = false; // 下拉刷新请求完成
         this.loading = false; // 上拉加载更多请求完成
       }
     },
-    loadImagesHeight(list) {
+    loadImagesHeight(data, list) {
       let count = 0; // 用来计数，表示是否所有图片高度已经获取
       list.forEach((item, index) => {
         // 创建图片对象，加载图片，计算图片高度
         const img = new Image();
-        img.src = item.cover;
+        img.src = this.$getImagePath(item.thumb);
         // eslint-disable-next-line no-multi-assign
         img.onload = img.onerror = e => {
           count++;
@@ -270,36 +179,35 @@ export default {
           }
           // 加载完成最后一个图片高度，开始下一步数据处理
           if (count === list.length) {
-            this.resolveDataList(list);
+            this.resolveDataList(data, list);
           }
         };
       });
     },
-    resolveDataList(list) {
+    resolveDataList(data, list) {
       // 处理数据
       // 下拉刷新，清空原数据
-      if (this.pageIndex <= 1) {
+      if (this.page <= 1) {
         this.itemCount = 0;
         this.dataList = [];
         this.lastRowHeights = [0, 0]; // 存储每列的最后一行高度清0
       }
-      if (list.length >= this.pageSize) {
-        this.pageIndex++; // 还有下一页
-      } else {
-        this.finished = true; // 当前tab类型下所有数据已经加载完成
-      }
+
       // 合并新老两个数组数据
       this.dataList = [...this.dataList, ...list];
       // 判断页面是否有数据
       this.haveData = this.dataList.length > 0 ? 2 : 1;
-      this.isLoading = true; // 下拉刷新请求完成
-      this.loading = true; // 上拉加载更多请求完成
       console.log('...datalist: ', this.dataList);
       console.log('...this.isLoading: ', this.isLoading);
       this.$nextTick(() => {
         setTimeout(() => {
           // 渲染完成，计算每个item宽高，设置标签坐标定位
           this.setItemElementPosition();
+          this.finished = this.page >= +data.totalPage;
+          if (this.page > +data.totalPage) {
+            return;
+          }
+          this.page += 1;
           this.isLoading = false; // 下拉刷新请求完成
           this.loading = false; // 上拉加载更多请求完成
         }, 1000);
